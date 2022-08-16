2a5142681de15d826ad344dba11b286a7f2e7d68
Aaron Liao <aiallen.cs07g@nctu.edu.tw>
2022-08-16 23:22:23 +0800

    modify files

--- 2_Circuits/Combinational Logic/Multiplexers/061_Mux2to1.v
+++ 2_Circuits/Combinational Logic/Multiplexers/061_Mux2to1.v
@@ -2,6 +2,18 @@ module top_module(
     input a, b, sel,
     output out ); 
 
+    // 寫法一
     assign out = sel ? b : a;
     
+    // 寫法二
+    assign out = (sel & b) | (~sel & a);	// Mux expressed as AND and OR
+
+    // 寫法三
+    always @(*) begin
+        if(sel)
+            out = b;
+        else
+            out = a;
+    end
+
 endmodule
--- 2_Circuits/Combinational Logic/Multiplexers/062_Mux2to1v.v
+++ 2_Circuits/Combinational Logic/Multiplexers/062_Mux2to1v.v
@@ -4,5 +4,13 @@ module top_module(
     output [99:0] out );
     
     assign out = sel ? b : a;
+    
+    // The following doesn't work. Why?
+	assign out = (sel & b) | (~sel & a);
+    // Because sel is one bit, we need to change the sel to a 100 bits bus
+    wire [99:0] bus;
+    assign bus = {100{sel}};
+    assign out = (bus & b) | (~bus & a);
+    
 
 endmodule
