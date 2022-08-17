d1d65378dec75867213de819133a51b5f0d88072
Aaron Liao <aiallen.cs07g@nctu.edu.tw>
2022-08-17 23:37:53 +0800

    add files

--- 2_Circuits/Combinational Logic/Arithmetic Circuits/069_m2014 q4j.v
+++ 2_Circuits/Combinational Logic/Arithmetic Circuits/069_m2014 q4j.v
@@ -1,3 +1,4 @@
+// Solution1
 module top_module (
     input [3:0] x,
     input [3:0] y, 
@@ -12,7 +13,27 @@ module top_module (
             sum[i] = x[i] ^ y[i] ^ cout[i-1];
             cout[i] = x[i] & y[i] | x[i] & cout[i-1] | y[i] & cout[i-1];
         end
-        sum[4] = cout[3];
+        sum[4] = cout[3];  //寫法一
     end
 
+    assign sum[4] = cout[3];  //寫法二
+
+endmodule
+
+
+// Solution2 (from HDLBits) 建議看看說明
+module top_module (
+	input [3:0] x,
+	input [3:0] y,
+	output [4:0] sum
+);
+
+	// This circuit is a 4-bit ripple-carry adder with carry-out.
+	assign sum = x+y;	// Verilog addition automatically produces the carry-out bit.
+
+	// Verilog quirk: Even though the value of (x+y) includes the carry-out, (x+y) is still considered to be a 4-bit number (The max width of the two operands).
+	// This is correct:
+	// assign sum = (x+y);
+	// But this is incorrect:
+	// assign sum = {x+y};	// Concatenation operator: This discards the carry-out
 endmodule
